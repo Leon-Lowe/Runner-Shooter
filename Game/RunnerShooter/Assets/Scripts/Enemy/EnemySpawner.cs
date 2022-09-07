8e9ab06e93d73f7a8a5b3b7ea430d75042113686
Leon Lowe <leonlowe0@outlook.com>
2022-09-07 11:33:08 +0100

    Max Enemies
    Added the following:
     - A cap to the max amount of enemies that can be on screen

--- Game/RunnerShooter/Assets/Scripts/Enemy/EnemySpawner.cs
+++ Game/RunnerShooter/Assets/Scripts/Enemy/EnemySpawner.cs
@@ -5,24 +5,35 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] GameObject enemyTemplate;
-    [SerializeField] EnemyData[] enemeis;
+    [SerializeField] EnemyData[] enemies;
+    [SerializeField] int maxEnemies = 10;
     [SerializeField] float maxPosX = 4f;
     [SerializeField] float ySpawnPos = 9f;
+    List<Transform> enemiesInGame;
+
+    void Awake()
+    {
+        enemiesInGame = new List<Transform>();
+    }
 
     void Start()
     {
-        CreateEnemy(ChooseRandomX(), ChooseRandomEnemy());    
+        CreateEnemy(ChooseRandomX(), ChooseRandomEnemy());
     }
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.L)){CreateEnemy(ChooseRandomX(), ChooseRandomEnemy());}
+        if(Input.GetKeyDown(KeyCode.L) && enemiesInGame.Count < maxEnemies)
+        {
+            CreateEnemy(ChooseRandomX(), ChooseRandomEnemy());
+        }
     }
 
     void CreateEnemy(float _xPos, EnemyData _enemyData)
     {
         GameObject newEnemy = Instantiate(enemyTemplate, new Vector2(_xPos, ySpawnPos), Quaternion.identity);
         newEnemy.GetComponent<Enemy>().SetEnemyData(_enemyData);
+        enemiesInGame.Add(newEnemy.transform);
     }
 
     float ChooseRandomX()
@@ -32,7 +43,7 @@ public class EnemySpawner : MonoBehaviour
 
     EnemyData ChooseRandomEnemy()
     {
-        int randEnemyIndex = Random.Range(0, enemeis.Length);
-        return enemeis[randEnemyIndex];
+        int randEnemyIndex = Random.Range(0, enemies.Length);
+        return enemies[randEnemyIndex];
     }
 }
